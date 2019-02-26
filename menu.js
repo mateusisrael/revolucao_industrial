565c075328982183c667418ad41a4a282898b70a
VaiNaWeb <vainaweb@gmail.com>
2019-02-26 12:30:02 -0300

    criando menu

---
+++ menu.js
@@ -0,0 +1,22 @@
+// Definindo as funções e variáveis
+var menu = document.getElementById("menu_icon");
+var container = document.getElementById("cont_menu");
+
+function menuClicado() {
+	console.log("Menu CLICADO");
+
+	var menu_aberto = document.createElement("div");
+	container.appendChild(menu_aberto);
+	// Observação IMPORTANTE:
+	// 	cada click cria uma nova div. se clicarmos
+	//  mil vzs no menu serão criadas mil divs.
+	// 	preciso definir um limite.
+	// Estilizando a div criada na função
+	menu_aberto.style = "position: fixed; width: 190%; height: 200%; background-color: blue;";
+}
+
+
+// Quando Clicar-mos no MENU
+menu.addEventListener('click', menuClicado);
+
+
